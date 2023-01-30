d2df8c31abb0fd1c1398e2a64df39e049e5cb219
victoryfox1116 <victoryfox1116@gmail.com>
2023-01-30 16:34:26 +0900

    Queen Move

---
+++ src/components/game.js
@@ -0,0 +1,263 @@
+import React from "react";
+
+import "../index.css";
+import Board from "./board.js";
+import King from "../pieces/king";
+import FallenSoldierBlock from "./fallen-soldier-block.js";
+import initialiseChessBoard from "../helpers/board-initialiser.js";
+import { transferNFT, initialiseNetwork } from "../helpers/blockchain-network";
+import { boardArray } from "../helpers/Wallet";
+
+export default class Game extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      squares: [],
+      whiteFallenSoldiers: [],
+      blackFallenSoldiers: [],
+      // squareArray: [
+      //   ["0xbe4ddb2c85c64a1892fb909e708ef0599e5ca6a1"],
+      //   ["0xdc5ff6988244ea53ed31c1dca81c8c5692ddd258"],
+      //   ["0x2f47cbc5838ce0cd691ba6b06bb45fbe73cbe4ec"],
+      //   ["0xdc49998a3b56e2f025d1d731acfcb1060b7ca60c"],
+      //   ["0xf012a6683d1d4597604bf60f878093f157785bd5"],
+      // ],
+      showArray: [],
+      player: 1,
+      sourceSelection: -1,
+      status: "",
+      turn: "white",
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    const initialSquares = await initialiseChessBoard();
+    const initialOwner = await initialiseNetwork();
+    this.state.showArray.push(initialOwner.owner);
+    this.setState({ squares: initialSquares });
+  }
+
+  async handleClick(i) {
+    const squares = [...this.state.squares];
+
+    if (this.state.sourceSelection === -1) {
+      if (!squares[i] || squares[i].player !== this.state.player) {
+        this.setState({
+          status:
+            "Wrong selection. Choose player " + this.state.player + " pieces.",
+        });
+        if (squares[i]) {
+          squares[i].style = { ...squares[i].style, backgroundColor: "" };
+        }
+      } else {
+        squares[i].style = {
+          ...squares[i].style,
+          backgroundColor: "RGB(111,143,114)",
+        }; // Emerald from http://omgchess.blogspot.com/2015/09/chess-board-color-schemes.html
+        this.setState({
+          status: "Choose destination for the selected piece",
+          sourceSelection: i,
+        });
+      }
+      return;
+    }
+
+    squares[this.state.sourceSelection].style = {
+      ...squares[this.state.sourceSelection].style,
+      backgroundColor: "",
+    };
+
+    if (squares[i] && squares[i].player === this.state.player) {
+      this.setState({
+        status: "Wrong selection. Choose valid Location again.",
+        sourceSelection: -1,
+      });
+    } else {
+      const whiteFallenSoldiers = [];
+      const blackFallenSoldiers = [];
+      const isDestEnemyOccupied = Boolean(squares[i]);
+      const isMovePossible = squares[this.state.sourceSelection].isMovePossible(
+        this.state.sourceSelection,
+        i,
+        isDestEnemyOccupied
+      );
+
+      if (isMovePossible) {
+        // switch (squares[this.state.sourceSelection].state.name) {
+        //   case "Bishop":
+        //     this.state.squareArray[0].push(
+        //       this.state.boardArray[Math.floor(i / 8)][i % 8]
+        //     );
+        //     break;
+        //   case "Knight":
+        //     this.state.squareArray[1].push(
+        //       this.state.boardArray[Math.floor(i / 8)][i % 8]
+        //     );
+        //     break;
+        //   case "King":
+        //     this.state.squareArray[2].push(
+        //       this.state.boardArray[Math.floor(i / 8)][i % 8]
+        //     );
+        //     break;
+        //   case "Queen":
+        //     this.state.squareArray[3].push(
+        //       this.state.boardArray[Math.floor(i / 8)][i % 8]
+        //     );
+        //     break;
+        //   case "Rook":
+        //     this.state.squareArray[4].push(
+        //       this.state.boardArray[Math.floor(i / 8)][i % 8]
+        //     );
+        //     break;
+        // }
+        this.setState({ loading: true });
+        let transferResult = await transferNFT(this.state.sourceSelection, i);
+        if (transferResult) {
+          this.state.showArray.push(boardArray[Math.floor(i / 8)][i % 8]);
+          if (squares[i] !== null) {
+            if (squares[i].player === 1) {
+              whiteFallenSoldiers.push(squares[i]);
+            } else {
+              blackFallenSoldiers.push(squares[i]);
+            }
+          }
+
+          squares[i] = squares[this.state.sourceSelection];
+          squares[this.state.sourceSelection] = null;
+
+          const isCheckMe = this.isCheckForPlayer(squares, this.state.player);
+
+          if (isCheckMe) {
+            this.setState((oldState) => ({
+              status:
+                "Wrong selection. Choose valid Location again. Now you have a check!",
+              sourceSelection: -1,
+            }));
+          } else {
+            // let player = this.state.player === 1 ? 2 : 1;
+            // let turn = this.state.turn === "white" ? "black" : "white";
+
+            this.setState((oldState) => ({
+              sourceSelection: -1,
+              squares,
+              whiteFallenSoldiers: [
+                ...oldState.whiteFallenSoldiers,
+                ...whiteFallenSoldiers,
+              ],
+              blackFallenSoldiers: [
+                ...oldState.blackFallenSoldiers,
+                ...blackFallenSoldiers,
+              ],
+              // player,
+              status: "",
+              // turn,
+            }));
+          }
+        }
+      } else {
+        this.setState({
+          status: "Wrong selection. Choose valid Location again.",
+          sourceSelection: -1,
+        });
+      }
+    }
+    this.setState({ loading: false });
+  }
+
+  getKingPosition(squares, player) {
+    return squares.reduce(
+      (acc, curr, i) =>
+        acc || //King may be only one, if we had found it, returned his position
+        (curr && //current squre mustn't be a null
+          curr.getPlayer() === player && //we are looking for aspecial king
+          curr instanceof King &&
+          i), // returned position if all conditions are completed
+      null
+    );
+  }
+
+  isCheckForPlayer(squares, player) {
+    const opponent = player === 1 ? 2 : 1;
+    const playersKingPosition = this.getKingPosition(squares, player);
+    const canPieceKillPlayersKing = (piece, i) =>
+      piece.isMovePossible(playersKingPosition, i, squares);
+    return squares.reduce(
+      (acc, curr, idx) =>
+        acc ||
+        (curr &&
+          curr.getPlayer() === opponent &&
+          canPieceKillPlayersKing(curr, idx) &&
+          true),
+      false
+    );
+  }
+
+  // changeOption(e) {
+  //   switch (e.target.value) {
+  //     case "Bishop":
+  //       this.setState({ showArray: this.state.squareArray[0] });
+  //       break;
+  //     case "Knight":
+  //       this.setState({ showArray: this.state.squareArray[1] });
+  //       break;
+  //     case "King":
+  //       this.setState({ showArray: this.state.squareArray[2] });
+  //       break;
+  //     case "Queen":
+  //       this.setState({ showArray: this.state.squareArray[3] });
+  //       break;
+  //     case "Rook":
+  //       this.setState({ showArray: this.state.squareArray[4] });
+  //       break;
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="d-flex">
+        <div className="game">
+          <div className="game-board">
+            {this.state.loading && <div className="loading">Processing...</div>}
+            <Board
+              squares={this.state.squares}
+              onClick={(i) => this.handleClick(i)}
+            />
+          </div>
+          <div className="game-info">
+            <div className="game-status">{this.state.status}</div>
+
+            <div className="fallen-soldier-block">
+              {
+                <FallenSoldierBlock
+                  whiteFallenSoldiers={this.state.whiteFallenSoldiers}
+                  blackFallenSoldiers={this.state.blackFallenSoldiers}
+                />
+              }
+            </div>
+          </div>
+        </div>
+        <div style={{ marginLeft: "30px" }}>
+          {/* <select
+                onChange={(e) => {
+                  this.changeOption(e);
+                }}
+              >
+                <option value="Bishop">Bishop</option>
+                <option value="Knight">Knight</option>
+                <option value="King">King</option>
+                <option value="Queen">Queen</option>
+                <option value="Rook">Rook</option>
+              </select> */}
+          {this.state.showArray.map((item, index) => {
+            return (
+              <div>
+                <label key={item}>{item}</label>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
---
+++ src/helpers/blockchain-network.js
@@ -0,0 +1,563 @@
+import { ethers } from "ethers";
+import { boardArray, privateArray } from "./Wallet.js";
+
+const nftabi = [
+  {
+    inputs: [],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "approved",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Approval",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        indexed: false,
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "ApprovalForAll",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "previousOwner",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "OwnershipTransferred",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        indexed: true,
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "Transfer",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "start",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "end",
+        type: "uint256",
+      },
+      {
+        internalType: "address",
+        name: "receiver",
+        type: "address",
+      },
+    ],
+    name: "SaveData",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "admin",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "approve",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+    ],
+    name: "balanceOf",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getApproved",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+    ],
+    name: "isApprovedForAll",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "name",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "ownerOf",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "renounceOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+      {
+        internalType: "bytes",
+        name: "data",
+        type: "bytes",
+      },
+    ],
+    name: "safeTransferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_adminAddr",
+        type: "address",
+      },
+    ],
+    name: "setAdmin",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "operator",
+        type: "address",
+      },
+      {
+        internalType: "bool",
+        name: "approved",
+        type: "bool",
+      },
+    ],
+    name: "setApprovalForAll",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "base",
+        type: "string",
+      },
+    ],
+    name: "setBaseURI",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "bytes4",
+        name: "interfaceId",
+        type: "bytes4",
+      },
+    ],
+    name: "supportsInterface",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "symbol",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "index",
+        type: "uint256",
+      },
+    ],
+    name: "tokenOfOwnerByIndex",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "tokenURI",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "totalSupply",
+    outputs: [
+      {
+        internalType: "uint256",
+        name: "",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "from",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "to",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "transferFrom",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+    ],
+    name: "transferOwnership",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://rpc-mumbai.maticvigil.com/",
+  80001
+);
+
+const contract_address = "0x7ef03d076f39ef49062e81ef5f2882828d9170f1";
+
+function getIndexOfK(arr, k) {
+  for (var i = 0; i < arr.length; i++) {
+    var index = arr[i].indexOf(k);
+    if (index > -1) {
+      return i * 8 + index;
+    }
+  }
+}
+
+function getNftContract(ethKey) {
+  const ethPrivkey = ethKey;
+  const wallet = new ethers.Wallet(ethPrivkey, provider);
+  // const signer = wallet.provider.getSigner(wallet.address);
+  // const signer = provider.getSigner();
+  const nftContract = new ethers.Contract(contract_address, nftabi, wallet);
+  return nftContract;
+}
+
+export async function initialiseNetwork() {
+  const myWallet =
+    "b0d16087ffe533fbcd641848d5d80894d0fe40b86344a08d999b78c81c2e0fc8";
+  const nftContract = getNftContract(myWallet);
+  const owner = await nftContract.ownerOf("3");
+  const number = await getIndexOfK(boardArray, owner);
+  return {
+    owner,
+    number,
+  };
+  // for (let i = 0; i < 64; i++) {
+  //   const randomWallet = ethers.Wallet.createRandom().connect(
+  //     window.ethersProvider
+  //   );
+  //   console.log(randomWallet.address, randomWallet.privateKey);
+  // }
+}
+
+export async function transferNFT(from, to) {
+  let privateKey = privateArray[Math.floor(from / 8)][from % 8];
+  const nftContract = getNftContract(privateKey);
+  let sendAddress = boardArray[Math.floor(from / 8)][from % 8];
+  let toAddress = boardArray[Math.floor(to / 8)][to % 8];
+  await nftContract.approve(toAddress, 3);
+  let result = await nftContract.transferFrom(sendAddress, toAddress, 3);
+  console.log(result);
+  return result;
+}
